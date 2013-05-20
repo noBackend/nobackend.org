1558ad7ce6aa3a1bc3ee7f93ab152ad20b5ac3df
Gregor Martynus <gregor@martynus.net>
2013-05-20 16:13:04 +0200

    fixed copy & paste bug

--- assets/main.js
+++ assets/main.js
@@ -26,16 +26,16 @@ $('document').ready( function() {
 // Analytics
 if( location.host === 'nobackend.org') {
   var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '519a2c84f5a1f516e9000011');
-    t.src = '//secure.gaug.es/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '519a2c84f5a1f516e9000011');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
 }
 
 // disqus
