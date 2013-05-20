2224490407f8517f14a03b730671654a759b6184
Gregor Martynus <gregor@martynus.net>
2013-05-20 16:06:04 +0200

    using gaug.es instead of evil

--- assets/main.js
+++ assets/main.js
@@ -23,15 +23,19 @@ $('document').ready( function() {
 });
 
 
-// Google Analytics
+// Analytics
 if( location.host === 'nobackend.org') {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40865648-1', 'nobackend.org');
-  ga('send', 'pageview');
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '519a2c84f5a1f516e9000011');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
 }
 
 // disqus
